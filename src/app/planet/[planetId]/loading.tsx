4335727425821c5631378758e684ef919bd0fbcc
MatheusCampos-450 <matheuscampos450@gmail.com>
2025-09-29 16:04:56 -0300

    feat: add global loading

---
+++ src/app/planet/[planetId]/loading.tsx
@@ -0,0 +1,9 @@
+import Spinner from '@/shared/components/ui/Spinner';
+
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-white/70 dark:bg-black/70">
+      <Spinner />
+    </div>
+  );
+}
