b18b234e6f760d8dd17e5fae9995a82f74e268c1
MatheusCampos-450 <matheuscampos450@gmail.com>
2025-09-30 13:17:02 -0300

    fix: remove unnecessary console.logs

--- src/@core/infra/http.ts
+++ src/@core/infra/http.ts
@@ -14,27 +14,4 @@ const http = axios.create({
   baseURL,
 });
 
-// =================================================================
-// 🚀 INTERCEPTOR DE REQUISIÇÃO PARA DEBUG
-// =================================================================
-
-http.interceptors.request.use(
-  (config) => {
-    const finalUrl = config.url;
-
-    console.log('AXIOS REQUEST DEBUG:', {
-      'Base URL Configurada': config.baseURL,
-      'URL Final Requisitada': finalUrl,
-      Method: config.method?.toUpperCase(),
-      'Headers de Autenticação': config.headers.Authorization || 'Nenhum',
-    });
-
-    return config;
-  },
-  (error) => {
-    console.error('AXIOS INTERCEPTOR ERROR (Pré-Envio):', error);
-    return Promise.reject(error);
-  },
-);
-
 export { http };
